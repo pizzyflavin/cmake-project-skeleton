0cfd7f9656dfba5e8318511359ae07d39d6b3159
pizzyflavin <pizzyflavin@gmail.com>
2021-09-12 00:57:53 -0400

    Only apply dead_strip flag if using AppleClang
    Checks for AppleClang as the C or CXX compiler before adding
    `-dead_strip` to the default linker flags.
    
    The linker flag `-dead_strip` passes check_linker_flag on GCC and causes
    a segfault on some systems. It seems like it parses `-de` from
    dead_strip and then tries to use `ad_strip` as another flag.

--- cmake/linker/DefaultLinkerSettings.cmake
+++ cmake/linker/DefaultLinkerSettings.cmake
@@ -4,10 +4,20 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/../compiler/CheckAndApplyFlags.cmake)
 
+
 set(desired_common_linker_optimization_flags
-    -Wl,-dead_strip # Strip dead symbols for macOS/OS X
     -Wl,-gc-sections # Strip dead symbols for GCC
 )
+
+# Only append dead_strip flag if on an Apple system. The flag is only used on
+# Apple systems, and it passes the check_linker_flag() test on non-Apple
+# systems and causes problems.
+if(CMAKE_C_COMPILER_ID STREQUAL "AppleClang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    set(desired_common_linker_optimization_flags
+        ${desired_common_linker_optimization_flags}
+        -Wl,-dead_strip # Strip dead symbols for macOS/OS X
+    )
+endif()
 
 if (CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
     apply_supported_linker_flags_globally(C desired_common_linker_optimization_flags)
